8bfa4368176943e1404dfce135bc98e98aaf3b5a
Abu Taher Siddiki Adnan <adnan02802@gamil.com>
2024-12-11 08:20:38 +0600

    Priority Queue using heap updated

--- Data_Structure/Priority_Queue(Using_Heap).cpp
+++ Data_Structure/Priority_Queue(Using_Heap).cpp
@@ -3,44 +3,60 @@ using namespace std;
 
 class Heap
 {
-    int heap[100000], SIZE = 0;
+    vector<int> heap;
+    int SIZE = 0;
 
 public:
-    void Insert(int x)
+    Heap(int n)
+    {
+        heap.resize(n + 1, 0);
+    }
+
+    void InsertMax(int x)
     {
         heap[++SIZE] = x;
         int n = SIZE;
-        while (n > 1)
+        while (n > 1 && heap[n / 2] < heap[n])
         {
-            if (heap[n / 2] < heap[n])
-                swap(heap[n / 2], heap[n]);
-            else
-                break;
+            swap(heap[n / 2], heap[n]);
             n /= 2;
         }
     }
 
-    int Delete()
+    void InsertMin(int x)
+    {
+        heap[++SIZE] = x;
+        int n = SIZE;
+        while (n > 1 && heap[n / 2] > heap[n])
+        {
+            swap(heap[n / 2], heap[n]);
+            n /= 2;
+        }
+    }
+
+    int DeleteMax()
     {
         if (SIZE == 0)
         {
             cout << "Heap is empty!" << endl;
             return -1;
         }
-        int root = heap[1];
-        heap[1] = heap[SIZE];
-        SIZE--;
 
+        int root = heap[1];
+        heap[1] = heap[SIZE--];
         int n = 1;
+
         while (2 * n <= SIZE)
         {
             int leftChild = 2 * n;
             int rightChild = 2 * n + 1;
             int largest = n;
+
             if (leftChild <= SIZE && heap[leftChild] > heap[largest])
                 largest = leftChild;
             if (rightChild <= SIZE && heap[rightChild] > heap[largest])
                 largest = rightChild;
+
             if (largest != n)
             {
                 swap(heap[n], heap[largest]);
@@ -49,7 +65,48 @@ public:
             else
                 break;
         }
+
         return root;
+    }
+
+    int DeleteMin()
+    {
+        if (SIZE == 0)
+        {
+            cout << "Heap is empty!" << endl;
+            return -1;
+        }
+
+        int root = heap[1];
+        heap[1] = heap[SIZE--];
+        int n = 1;
+
+        while (2 * n <= SIZE)
+        {
+            int leftChild = 2 * n;
+            int rightChild = 2 * n + 1;
+            int smallest = n;
+
+            if (leftChild <= SIZE && heap[leftChild] < heap[smallest])
+                smallest = leftChild;
+            if (rightChild <= SIZE && heap[rightChild] < heap[smallest])
+                smallest = rightChild;
+
+            if (smallest != n)
+            {
+                swap(heap[n], heap[smallest]);
+                n = smallest;
+            }
+            else
+                break;
+        }
+
+        return root;
+    }
+
+    int getFront()
+    {
+        return SIZE > 0 ? heap[1] : -1;
     }
 
     bool isEmpty()
@@ -70,46 +127,91 @@ class PriorityQueue
     Heap h;
 
 public:
-    void enqueue(int x)
+    PriorityQueue(int n) : h(n) {}
+
+    void enqueueMax(int x)
     {
-        h.Insert(x);
+        h.InsertMax(x);
     }
 
-    int dequeue()
+    void enqueueMin(int x)
     {
-        return h.Delete();
+        h.InsertMin(x);
     }
 
-    bool isEmpty()
+    int dequeueMax()
     {
-        return h.isEmpty();
+        return h.DeleteMax();
+    }
+
+    int dequeueMin()
+    {
+        return h.DeleteMin();
+    }
+
+    int getFront()
+    {
+        return h.getFront();
     }
 
     void display()
     {
         h.display();
     }
+
+    bool isEmpty()
+    {
+        return h.isEmpty();
+    }
 };
 
 int main()
 {
-    PriorityQueue pq;
-    pq.enqueue(44);
-    pq.enqueue(30);
-    pq.enqueue(50);
-    pq.enqueue(22);
-    pq.enqueue(60);
-    pq.enqueue(55);
-    pq.enqueue(77);
-    pq.enqueue(55);
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    cout << "Choose Priority Queue type:\n1. Max Heap\n2. Min Heap\n";
+    int choice;
+    cin >> choice;
 
-    cout << "Priority Queue: ";
-    pq.display();
+    PriorityQueue pq(n);
 
-    cout << "Dequeued: " << pq.dequeue() << endl;
+    if (choice == 1)
+    {
+        cout << "Enter elements for Max Heap: \n";
+        for (int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+            pq.enqueueMax(x);
+        }
+        cout << "Max Heap: ";
+        pq.display();
 
-    cout << "Priority Queue after dequeue: ";
-    pq.display();
+        cout << "Dequeued element: " << pq.dequeueMax() << endl;
+        cout << "Max Heap after dequeue: ";
+        pq.display();
+    }
+    else if (choice == 2)
+    {
+        cout << "Enter elements for Min Heap: \n";
+        for (int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+            pq.enqueueMin(x);
+        }
+        cout << "Min Heap: ";
+        pq.display();
+
+        cout << "Dequeued element: " << pq.dequeueMin() << endl;
+        cout << "Min Heap after dequeue: ";
+        pq.display();
+    }
+    else
+    {
+        cout << "Invalid choice!" << endl;
+    }
 
     return 0;
 }
