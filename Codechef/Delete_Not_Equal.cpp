668f3b00b5f016adb3aa8daa597f6d9f22590e4a
Abu Taher Siddiki Adnan <adnan02802@gamil.com>
2025-01-01 22:11:56 +0600

    Code Accepted

---
+++ Codechef/Delete_Not_Equal.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define all(a) a.begin(), a.end()
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define rev(n) n.rbegin(), n.rend()
+#define sz(x) (int)x.size()
+#define pb push_back
+#define Y cout << "YES" << endl
+#define N cout << "NO" << endl
+#define print(n) cout << n << endl
+#define Pi M_PI
+#define newline cout << endl
+#define vecIn(v, n)             \
+    vll v;                      \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        ll x;                   \
+        cin >> x;               \
+        v.pb(x);                \
+    };
+#define vecOut(v, n)            \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cout << v[i] << " ";    \
+    }                           \
+    cout << endl;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int length;
+        string binary_string;
+        cin >> length >> binary_string;
+        int count_zeros = 0, count_ones = 0;
+
+        for (char character : binary_string)
+        {
+            if (character == '0')
+                count_zeros++;
+            else if (character == '1')
+                count_ones++;
+        }
+        if (count_zeros == length || count_ones == length)
+            print(length);
+        else
+            print(1);
+    }
+
+    return 0;
+}
