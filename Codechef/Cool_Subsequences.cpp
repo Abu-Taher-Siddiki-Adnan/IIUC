edb5b90c48a340f5f5bc61ca7a9791e1b37842ee
Abu Taher Siddiki Adnan <adnan02802@gamil.com>
2025-02-12 21:56:07 +0600

    Code accepted

---
+++ Codechef/Cool_Subsequences.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define all(a) a.begin(), a.end()
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define rev(n) n.rbegin(), n.rend()
+#define sz(x) (int)x.size()
+#define pb push_back
+#define Y cout << "YES" << endl
+#define N cout << "NO" << endl
+#define print(n) cout << n << endl
+#define Pi M_PI
+#define newline cout << endl
+#define vecIn(v, n)             \
+    vll v;                      \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        ll x;                   \
+        cin >> x;               \
+        v.pb(x);                \
+    };
+#define vecOut(v, n)            \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cout << v[i] << " ";    \
+    }                           \
+    cout << endl;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vecIn(vec, n);
+        vi a(n);
+        unordered_map<int, int> freq;
+        vi res;
+        for (int i = 0; i < n; ++i)
+        {
+            freq[vec[i]]++;
+            if (freq[vec[i]] > 1 && res.empty())
+                res.pb(vec[i]);
+        }
+        if (!res.empty())
+        {
+            print(sz(res));
+            vecOut(res, sz(res));
+        }
+        else
+            print(-1);
+    }
+    return 0;
+}
---
+++ Codechef/Time_Penalty.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define all(a) a.begin(), a.end()
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define rev(n) n.rbegin(), n.rend()
+#define sz(x) (int)x.size()
+#define pb push_back
+#define Y cout << "YES" << endl
+#define N cout << "NO" << endl
+#define print(n) cout << n << endl
+#define Pi M_PI
+#define newline cout << endl
+#define vecIn(v, n)             \
+    vll v;                      \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        ll x;                   \
+        cin >> x;               \
+        v.pb(x);                \
+    };
+#define vecOut(v, n)            \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cout << v[i] << " ";    \
+    }                           \
+    cout << endl;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int x, y;
+    cin >> x >> y;
+    print(x + 10 * y);
+
+    return 0;
+}
