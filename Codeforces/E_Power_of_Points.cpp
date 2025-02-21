a8c04f32d461247fdf2ea992cbcbdb18adfa7906
Abu Taher Siddiki Adnan <adnan02802@gmail.com>
2025-02-22 00:11:11 +0600

    Code Updated

---
+++ Codeforces/E_Power_of_Points.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define all(a) a.begin(), a.end()
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define rev(n) n.rbegin(), n.rend()
+#define sz(x) (int)x.size()
+#define pb push_back
+#define Yes cout << "YES" << endl
+#define No cout << "NO" << endl
+#define print(n) cout << n << endl
+#define Pi M_PI
+#define newline cout << endl
+
+#define vecIn(v, n)             \
+    vll v;                      \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        ll x;                   \
+        cin >> x;               \
+        v.pb(x);                \
+    };
+
+#define vecOut(v, n)            \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cout << v[i] << " ";    \
+    }                           \
+    cout << endl;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> p(n);
+        unordered_map<int, int> freq;
+
+        for (int i = 0; i < n; i++)
+        {
+            cin >> p[i];
+            freq[p[i]]++;
+        }
+
+        long long ts = 0;
+
+        for (auto i : freq)
+        {
+            ts += i.second * (i.second - 1) / 2;
+        }
+
+        cout << ts << endl;
+    }
+    return 0;
+}
