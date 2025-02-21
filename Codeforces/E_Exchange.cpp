0d2de424cd1c4589cd733d2d6d150debcf45c1c8
Abu Taher Siddiki Adnan <adnan02802@gmail.com>
2025-02-21 12:30:45 +0600

    Code Accepted

---
+++ Codeforces/E_Exchange.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define all(a) a.begin(), a.end()
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define rev(n) n.rbegin(), n.rend()
+#define sz(x) (int)x.size()
+#define pb push_back
+#define Yes cout << "YES" << endl
+#define No cout << "NO" << endl
+#define print(n) cout << n << endl
+#define Pi M_PI
+#define newline cout << endl
+#define vecIn(v, n)             \
+    vll v;                      \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        ll x;                   \
+        cin >> x;               \
+        v.pb(x);                \
+    };
+#define vecOut(v, n)            \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cout << v[i] << " ";    \
+    }                           \
+    cout << endl;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, a, b;
+        cin >> n >> a >> b;
+        if (a <= b)
+        {
+            int ans = (n + a - 1) / a;
+            print(ans);
+        }
+        else
+            print(1);
+    }
+    return 0;
+}
