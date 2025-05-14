f1f87042914882a63dc5af2786a38e5f0fd61c5e
Abu Taher Siddiki Adnan <adnan02802@gmail.com>
2025-05-15 01:57:10 +0600

    Code Accepted

---
+++ Codeforces/B_Card_Constructions.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define all(a) a.begin(), a.end()
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define pi pair<int, int>
+#define pll pair<ll, ll>
+#define vPi vector<pair<int, int>>
+#define vPll vector<pair<ll, ll>>
+#define uMap unordered_map<int, int>
+#define rev(n) n.rbegin(), n.rend()
+#define sz(x) (int)x.size()
+#define pb push_back
+#define Yes cout << "YES" << endl
+#define No cout << "NO" << endl
+#define print(n) cout << n << endl
+#define Pi M_PI
+#define newline cout << endl
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define F0R(i, n) FOR(i, 0, n)
+#define fast_io                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define vecIn(v, n, type)       \
+    vector<type> v(n);          \
+    for (int i = 0; i < n; i++) \
+        cin >> v[i];
+#define vecOut(v)         \
+    for (auto &x : v)     \
+        cout << x << " "; \
+    cout << endl;
+
+void solve()
+{
+    vi f(30001);
+    f[1] = 2;
+    FOR(i, 2, 30001)
+       f[i] = f[i - 1] + 3 * i - 1;
+       
+    int n;
+    cin >> n;
+    int now = 30000, ans = 0;
+    while (f[now] > n)
+        now--;
+    while (now)
+    {
+        while (n >= f[now])
+        {
+            ans++;
+            n -= f[now];
+        }
+        now--;
+    }
+    print(ans);
+}
+
+int main()
+{
+    fast_io;
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
