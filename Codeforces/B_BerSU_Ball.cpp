a03d19dd214a3ec10956af43ada847e3765c2ab9
Abu Taher Siddiki Adnan <adnan02802@gmail.com>
2025-06-21 23:44:29 +0600

    Code Accepted

---
+++ Codeforces/B_BerSU_Ball.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define all(a) a.begin(), a.end()
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define pi pair<int, int>
+#define pll pair<ll, ll>
+#define vPi vector<pair<int, int>>
+#define vPll vector<pair<ll, ll>>
+#define uMap unordered_map<int, int>
+#define rev(n) n.rbegin(), n.rend()
+#define sz(x) (int)x.size()
+#define pb push_back
+#define Yes cout << "YES" << endl
+#define No cout << "NO" << endl
+#define print(n) cout << n << endl
+#define Pi M_PI
+#define newline cout << endl
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define F0R(i, n) FOR(i, 0, n)
+#define fast_io                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define vecIn(v, n, type)       \
+    vector<type> v(n);          \
+    for (int i = 0; i < n; i++) \
+        cin >> v[i];
+
+#define vecOut(v)         \
+    for (auto &x : v)     \
+        cout << x << " "; \
+    cout << endl;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vecIn(a, n, int);
+    int m;
+    cin >> m;
+    vecIn(b, m, int);
+    sort(all(a));
+    sort(all(b));
+    int i = 0, j = 0;
+    int ans = 0;
+    while (i < n and j < m)
+    {
+        if (abs(a[i] - b[j]) <= 1)
+        {
+            ans++;
+            i++;
+            j++;
+        }
+        else if (a[i] < b[j])
+            i++;
+        else
+            j++;
+    }
+    print(ans);
+}
+int main()
+{
+    fast_io;
+
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
