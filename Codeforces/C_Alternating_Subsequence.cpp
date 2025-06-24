e4897b0ab217cf841707ee1fc010753c34aff4d2
Abu Taher Siddiki Adnan <adnan02802@gmail.com>
2025-06-24 22:54:20 +0600

    Code Accepted

---
+++ Codeforces/C_Alternating_Subsequence.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define all(a) a.begin(), a.end()
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define pi pair<int, int>
+#define pll pair<ll, ll>
+#define vPi vector<pair<int, int>>
+#define vPll vector<pair<ll, ll>>
+#define uMap unordered_map<int, int>
+#define rev(n) n.rbegin(), n.rend()
+#define sz(x) (int)x.size()
+#define pb push_back
+#define Yes cout << "YES" << endl
+#define No cout << "NO" << endl
+#define print(n) cout << n << endl
+#define Pi M_PI
+#define newline cout << endl
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define F0R(i, n) FOR(i, 0, n)
+#define fast_io                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define vecIn(v, n, type)       \
+    vector<type> v(n);          \
+    for (int i = 0; i < n; i++) \
+        cin >> v[i];
+
+#define vecOut(v)         \
+    for (auto &x : v)     \
+        cout << x << " "; \
+    cout << endl;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vecIn(a, n, ll);
+    ll ans = 0;
+    F0R(i, n)
+    {
+        if (a[i] < 0)
+        {
+            ll mx = a[i], j = i + 1;
+            while (j < n && a[j] < 0)
+            {
+                mx = max(mx, a[j]);
+                j++;
+            }
+            ans += mx;
+            i = j - 1;
+        }
+        else
+        {
+            ll mx = a[i], j = i + 1;
+            while (j < n && a[j] > 0)
+            {
+                mx = max(mx, a[j]);
+                j++;
+            }
+            ans += mx;
+            i = j - 1;
+        }
+    }
+    print(ans);
+}
+int main()
+{
+    fast_io;
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
