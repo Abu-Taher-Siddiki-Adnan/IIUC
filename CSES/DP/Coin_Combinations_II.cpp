af36cd44a11881563070ac7ee8e49c0e46998ab1
Abu Taher Siddiki Adnan <adnan02802@gmail.com>
2025-07-10 22:37:01 +0600

    Code Accepted

---
+++ CSES/DP/Coin_Combinations_II.cpp
@@ -0,0 +1,87 @@
+/***********************************************************
+ *                  Abu Taher Siddiki Adnan                *
+ ***********************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<ll>
+#define pi pair<int, int>
+#define pll pair<ll, ll>
+#define vPi vector<pair<int, int>>
+#define vPll vector<pair<ll, ll>>
+#define Map map<int, int>
+#define uMap unordered_map<int, int>
+#define Set set<int>
+#define uSet unordered_set<int>
+
+#define all(a) a.begin(), a.end()
+#define rev(n) n.rbegin(), n.rend()
+#define sz(x) (int)x.size()
+#define pb push_back
+#define Yes cout << "YES" << endl
+#define No cout << "NO" << endl
+#define print(n) cout << n << endl
+#define Pi M_PI
+#define ff first
+#define ss second
+#define newline cout << endl
+
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define F0R(i, n) FOR(i, 0, n)
+#define RFOR(i, a, b) for (int i = (a); i >= (b); i--)
+
+#define fast_io                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define vecIn(v, n, type)       \
+    vector<type> v(n);          \
+    for (int i = 0; i < n; i++) \
+        cin >> v[i];
+
+#define vecOut(v)         \
+    for (auto &x : v)     \
+        cout << x << " "; \
+    cout << endl;
+const int MOD = 1e9 + 7;
+void solve()
+{
+    int n, x;
+    cin >> n >> x;
+    vecIn(coins, n, int);
+    vi dp(x + 1, -1);
+    dp[0] = 1;
+    F0R(i, n)
+    {
+        for (int j = coins[i]; j <= x; j++)
+        {
+            if (dp[j - coins[i]] != -1)
+            {
+                if (dp[j] == -1)
+                    dp[j] = 0;
+                dp[j] = (dp[j] + dp[j - coins[i]]) % MOD;
+
+            }
+        }
+    }
+    if (dp[x] == -1)
+        print(0);
+    else
+        print(dp[x]);
+}
+int main()
+{
+    fast_io;
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
